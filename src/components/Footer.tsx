153860abcdad5ff0c18b9b3252fabcf50904cb20
Robin Goodwin <robin@novadiem.com>
2025-05-27 03:22:07 -0700

    update fooder

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,8 +1,10 @@
 export default function Footer() {
-    return (
-      <footer>
-        <p>© {new Date().getFullYear()} FOAF Foundation</p>
-      </footer>
-    );
-  }
+  return (
+    <footer className="flex items-center justify-center text-center py-6">
+      <span>
+        &copy; {new Date().getFullYear()} FOAF Foundation. All rights reserved.
+      </span>
+    </footer>
+  );
+}
   
